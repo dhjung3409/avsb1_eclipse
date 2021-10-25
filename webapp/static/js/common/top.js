c44fed746a565dcb02058f874f18cd923d638e3b
javatndjqid <javatndjqid@gmail.com>
2021-10-25 14:26:43 +0900

    2021/10/25 14:26

---
+++ webapp/static/js/common/top.js
@@ -0,0 +1,43 @@
+function getUserInfo(){
+    $.ajax({
+        url:"/etc/license/user/check",
+        method:"GET",
+        data:{},
+        error:function(err_res){
+
+        },
+        success:function(suc_res){
+            console.log(suc_res)
+            setUserInfo(suc_res)
+        }
+    })
+}
+function setUserInfo(data) {
+    var userName = document.getElementById("muserName");
+    var nameText = document.createTextNode(data["userInfo"]["userName"]);
+    var userRole = document.getElementById("muserRole");
+
+    var role;
+
+    if (data["userRole"][0]["authority"] === "Role_admin") {
+        role = "admin";
+    } else {
+        role = "user";
+    }
+
+    var roleText = document.createTextNode(role);
+    var licenseRemain = data["licenseStatus"];
+    if(licenseRemain<=0){
+
+    }else{
+        var license = document.createTextNode("라이센스 만료 "+licenseRemain+"일 전");
+        document.getElementById("license").appendChild(license);
+    }
+    userName.appendChild(nameText);
+    userRole.appendChild(roleText);
+}
+
+function topInit(){
+    getUserInfo();
+}
+topInit();
